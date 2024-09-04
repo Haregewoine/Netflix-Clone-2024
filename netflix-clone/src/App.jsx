a7faff5fb8f4d8aa98df2db95e4c9a0bfcd4992f
Haregewoine <Haregewoineabire9@gmail.com>
2024-09-03 23:41:01 -0400

    Header and  Footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-
-
 import './App.css'
+import Home from './Pages/Home/Home';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
-
-    </div>
-  )
+    <>
+      <div className="App">
+       <Home/>
+      </div>
+    </>
+  );
 }
 
 export default App
